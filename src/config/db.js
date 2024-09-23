a7bd82ad9c0976a5a6465744c90ab8f802abcdfe
cineprime837 <161740210+cineprime837@users.noreply.github.com>
2024-09-23 21:43:42 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-const uri=encodeURIComponent(process.env.KeyId);
 
 const connect = () =>{
     return (mongoose.connect(`mongodb+srv://kiran:kirankumar@cluster0.jwmqg.mongodb.net/`))
